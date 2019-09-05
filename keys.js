954e2e33c71d8e8035f2a4f35a3b10c8270621cd
Ricardo Cabrera <ricky_92rc@hotmail.com>
2019-09-04 23:54:18 -0700

    installing package and playing with request

--- keys.js
+++ keys.js
@@ -4,4 +4,3 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-var spotify = new Spotify(keys.spotify);
--- liri.js
+++ liri.js
@@ -1,3 +1,19 @@
-require("dotenv").config();
+// require("dotenv").config();
+var request = require('request');
+
+
+request(`https://www.omdbapi.com/?t=${process.argv[3]}&y=&plot=short&apikey=trilogy`, function (Title, Year, Rating, Rotten, Country, Language, Plot, Actors){
+    console.log('Title:', Title);
+    console.log('Year:', Year);
+    console.log('Rating:', Rating);
+    console.log('Rotten Tomatoes:', Rotten);
+    console.log('Country:', Country);
+    console.log('Language:', Language);
+    console.log('Plot:', Plot);
+    console.log('Actors:', Actors);
+});
 
 var keys = require("./keys.js");
+
+// var spotify = new Spotify(keys.spotify);
+
